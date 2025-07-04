8eef7ba9c7dc751ece6740c9ea55c2ed45c168ec
Tibyan Hussien <37901881+Bluerate90@users.noreply.github.com>
2025-07-04 14:29:13 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,27 @@
+# Instance Information
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.web.public_ip
+}
 
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.web.id
+}
+
+# SSH Connection
+output "ssh_command" {
+  description = "SSH command to connect to the instance"
+  value       = "ssh -i royal-hotel-key.pem ec2-user@${aws_instance.web.public_ip}"
+}
+
+# Infrastructure Details
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = aws_security_group.web.id
+}
